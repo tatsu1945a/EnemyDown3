339bcd3040633adda3f33d5e71c4017a595eaa8d
Tatsuhiro Akita <vmak8160@gmail.com>
2024-01-02 22:54:47 +0900

    day18 commit

--- src/main/java/plugin/enemydown/command/EnemyDownCommand.java
+++ src/main/java/plugin/enemydown/command/EnemyDownCommand.java
@@ -28,7 +28,6 @@ public class EnemyDownCommand implements CommandExecutor, Listener {
 
   private Main main;
   private List<PlayerScore> playerScoreList = new ArrayList<>();
-  private int gameTime = 20;
 
   public EnemyDownCommand(Main main) {
     this.main = main;
@@ -36,30 +35,25 @@ public class EnemyDownCommand implements CommandExecutor, Listener {
 
   @Override
   public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-
     if (sender instanceof Player player) {
-      if(playerScoreList.isEmpty()) {
-        addNewPlayer(player);
-      } else {
-        for(PlayerScore playerScore : playerScoreList) {
-          if(!playerScore.getPlayerName().equals(player.getName())) {
-            addNewPlayer(player);
-          }
-        }
-      }
-      gameTime = 20;
+      PlayerScore nowPlayer = getPlayerScore(player);
+      nowPlayer.setGameTime(20);
+
       World world = player.getWorld();
 
       initPlayerStatus(player);
 
       Bukkit.getScheduler().runTaskTimer(main, Runnable -> {
-        if(gameTime <= 0) {
+        if(nowPlayer.getGameTime() <= 0) {
           Runnable.cancel();
-          player.sendMessage("ゲーム終了しました");
+          player.sendTitle("ゲーム終了しました",
+              nowPlayer.getPlayerName() + " 合計" + nowPlayer.getScore() + "点",
+              0,30,0);
+          nowPlayer.setScore(0);
           return;
         }
         world.spawnEntity(getEnemySpawnLocation(player, world), getEnemy());
-        gameTime -= 5;
+        nowPlayer.setGameTime(nowPlayer.getGameTime() -5);
       },0,5*20);
 
     }
@@ -83,13 +77,36 @@ public class EnemyDownCommand implements CommandExecutor, Listener {
   }
 
   /**
+   * 現在実行しているプレイヤーの情報を取得する
+   *
+   * @param player コマンドを実行したプレイヤー
+   * @return 現在実行しているプレイヤーのスコア情報
+   */
+  private PlayerScore getPlayerScore(Player player) {
+    if(playerScoreList.isEmpty()) {
+      return addNewPlayer(player);
+    } else {
+      for(PlayerScore playerScore : playerScoreList) {
+        if(!playerScore.getPlayerName().equals(player.getName())) {
+          return addNewPlayer(player);
+        } else {
+          return playerScore;
+        }
+      }
+    }
+    return null;
+  }
+
+  /**
    * 新規のプレイヤー情報をリストに追加します
    * @param player コマンドを実行したプレイヤー
+   * @return 新規プレーヤー
    */
-  private void addNewPlayer(Player player) {
+  private PlayerScore addNewPlayer(Player player) {
     PlayerScore newPlayer = new PlayerScore();
     newPlayer.setPlayerName(player.getName());
     playerScoreList.add(newPlayer);
+    return newPlayer;
   }
 
   /**
--- src/main/java/plugin/enemydown/data/PlayerScore.java
+++ src/main/java/plugin/enemydown/data/PlayerScore.java
@@ -13,4 +13,5 @@ import org.bukkit.entity.Player;
 public class PlayerScore {
   private String playerName;
   private int score;
+  private int gameTime;
 }
