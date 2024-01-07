457ec2642cfe59805bd1b32273fdf8a8cf433de6
Tatsuhiro Akita <vmak8160@gmail.com>
2024-01-07 22:23:18 +0900

    day20 commit

--- src/main/java/plugin/enemydown/command/EnemyDownCommand.java
+++ src/main/java/plugin/enemydown/command/EnemyDownCommand.java
@@ -13,6 +13,7 @@ import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
@@ -49,8 +50,14 @@ public class EnemyDownCommand implements CommandExecutor, Listener {
           Runnable.cancel();
           player.sendTitle("ゲーム終了しました",
               nowPlayer.getPlayerName() + " 合計" + nowPlayer.getScore() + "点",
-              0,30,0);
+              0,60,0);
           nowPlayer.setScore(0);
+          List<Entity> nearbyEntities = player.getNearbyEntities(50, 0, 50);
+          for(Entity enemy: nearbyEntities) {
+            switch (enemy.getType()) {
+              case ZOMBIE, SKELETON, WITCH -> enemy.remove();
+            }
+          }
           return;
         }
         world.spawnEntity(getEnemySpawnLocation(player, world), getEnemy());
